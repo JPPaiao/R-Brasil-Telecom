28724d59df59a02468a24a41138069cc05c8e8f0
jplp <jp.paiao03@gmail.com>
2023-08-11 16:44:44 -0300

    Adicionado as imagens do carrosel

--- css/tailwind.config.js
+++ css/tailwind.config.js
@@ -1,6 +1,44 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {}
 
 /**
- * Por conta de extensão não consigo fazer as configurações por aqui, coloquei tudo no config.js
+ * Para fazer a extensão Tailwind IntelliSense funcionar, 
+ * tenho que ter esse arquivo.
+ * 
+ * Mas as configurações do Tailwind estão no arquivo config.js
  */
+
+module.exports = {
+    content: ['../*.{html,js}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                'roboto': 'Roboto',
+                'josefin': 'Josefin Sans'
+            },
+            textColor: {
+                green: {
+                primary: '#0AD11B',
+                secondary: '#88EB90',
+                tertiary: '#089E14',
+                background: '#04520B',
+                }
+            },
+            backgroundColor: {
+                'background': '#212429',
+                green: {
+                primary: '#0AD11B',
+                secondary: '#88EB90',
+                tertiary: '#089E14',
+                background: '#04520B',
+                },
+            },
+            borderColor: {
+                'green-background': '#04520B',
+                'green-primary': '#0AD11B',
+                'green-secondary': '#88EB90',
+                'green-tertiary': '#089E14'
+            }
+        },
+    },
+    plugins: [],
+}
